3cd3abae17270d7cbf992fb97041de4cb554f710
yusuke1225math2 <63696752+yusuke1225math2@users.noreply.github.com>
2025-07-03 22:00:36 +0900

    useragent added

--- main.py
+++ main.py
@@ -6,28 +6,33 @@
 # 1. ライブラリの読み込み
 ############################################################
 # 「.env」ファイルから環境変数を読み込むための関数
-from dotenv import load_dotenv
 # ログ出力を行うためのモジュール
 import logging
+import os
+
 # streamlitアプリの表示を担当するモジュール
 import streamlit as st
-# （自作）画面表示以外の様々な関数が定義されているモジュール
-import utils
-# （自作）アプリ起動時に実行される初期化処理が記述された関数
-from initialize import initialize
+from dotenv import load_dotenv
+
 # （自作）画面表示系の関数が定義されているモジュール
 import components as cn
+
 # （自作）変数（定数）がまとめて定義・管理されているモジュール
 import constants as ct
 
+# （自作）画面表示以外の様々な関数が定義されているモジュール
+import utils
+
+# （自作）アプリ起動時に実行される初期化処理が記述された関数
+from initialize import initialize
+
+os.environ["USER_AGENT"] = "myagent"
 
 ############################################################
 # 2. 設定関連
 ############################################################
 # ブラウザタブの表示文言を設定
-st.set_page_config(
-    page_title=ct.APP_NAME
-)
+st.set_page_config(page_title=ct.APP_NAME)
 
 # ログ出力を行うためのロガーの設定
 logger = logging.getLogger(ct.LOGGER_NAME)
@@ -76,7 +81,9 @@ except Exception as e:
     # エラーログの出力
     logger.error(f"{ct.CONVERSATION_LOG_ERROR_MESSAGE}\n{e}")
     # エラーメッセージの画面表示
-    st.error(utils.build_error_message(ct.CONVERSATION_LOG_ERROR_MESSAGE), icon=ct.ERROR_ICON)
+    st.error(
+        utils.build_error_message(ct.CONVERSATION_LOG_ERROR_MESSAGE), icon=ct.ERROR_ICON
+    )
     # 後続の処理を中断
     st.stop()
 
@@ -115,10 +122,13 @@ if chat_message:
             # エラーログの出力
             logger.error(f"{ct.GET_LLM_RESPONSE_ERROR_MESSAGE}\n{e}")
             # エラーメッセージの画面表示
-            st.error(utils.build_error_message(ct.GET_LLM_RESPONSE_ERROR_MESSAGE), icon=ct.ERROR_ICON)
+            st.error(
+                utils.build_error_message(ct.GET_LLM_RESPONSE_ERROR_MESSAGE),
+                icon=ct.ERROR_ICON,
+            )
             # 後続の処理を中断
             st.stop()
-    
+
     # ==========================================
     # 7-3. LLMからの回答表示
     # ==========================================
@@ -137,14 +147,17 @@ if chat_message:
             elif st.session_state.mode == ct.ANSWER_MODE_2:
                 # 入力に対しての回答と、参照した文書のありかを表示
                 content = cn.display_contact_llm_response(llm_response)
-            
+
             # AIメッセージのログ出力
             logger.info({"message": content, "application_mode": st.session_state.mode})
         except Exception as e:
             # エラーログの出力
             logger.error(f"{ct.DISP_ANSWER_ERROR_MESSAGE}\n{e}")
             # エラーメッセージの画面表示
-            st.error(utils.build_error_message(ct.DISP_ANSWER_ERROR_MESSAGE), icon=ct.ERROR_ICON)
+            st.error(
+                utils.build_error_message(ct.DISP_ANSWER_ERROR_MESSAGE),
+                icon=ct.ERROR_ICON,
+            )
             # 後続の処理を中断
             st.stop()
 
